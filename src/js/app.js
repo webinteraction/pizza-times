ad255c5ba33ae9f509cc3b21c99f1b004949fedf
kyleschaeffer <kyle@spirito.us>
2019-06-04 21:14:42 -0400

    Add aria-expanded attribute, entry animation, toggle icon

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,4 @@
 import { Toggle } from './toggle'
 
 // Toggle buttons
-const toggleBtns = new Toggle({
-  onToggle: (btn, target, isToggled) => {
-    btn.textContent = isToggled ? '➖' : '➕'
-  },
-})
+const toggleBtns = new Toggle()
