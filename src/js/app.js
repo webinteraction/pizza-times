61a185aa6bdfbce6af793a7c93c411320f05bd04
kyleschaeffer <kyle@spirito.us>
2019-06-04 21:31:01 -0400

    Add dismissibles

--- src/js/app.js
+++ src/js/app.js
@@ -2,3 +2,9 @@ import { Toggle } from './toggle'
 
 // Toggle buttons
 const toggleBtns = new Toggle()
+
+// Dismissibles
+const dismissibles = new Toggle({
+  attr: 'data-dismiss',
+  toggleContainer: '.dismissible',
+})
